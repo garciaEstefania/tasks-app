fd905534c163ffe65e05509aad1cdc72d94ed75e
Estefania Garcia <garcia_estefannia@hotmail.com>
2024-06-20 17:12:01 -0600

    Add alias config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+        alias: {
+          '~assets': './src/assets',
+          '~components': './src/components',
+          '~interfaces': './src/interfaces',
+          '~navigation': './src/navigation',
+          '~redux': './src/redux',
+          '~screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
